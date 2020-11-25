34f6a351dd25148d44709e2f6f1fdde6d48b61c3
Brian Cross <briancross@shaw.ca>
2020-11-25 16:18:51 -0700

    Add cleaner solution.

--- 1-replace-with-alphabet-position/index.js
+++ 1-replace-with-alphabet-position/index.js
@@ -1,15 +1,30 @@
-function alphabetPosition(str) {
-  const positions = "abcdefghijklmnopqrstuvwxyz".split("");
-  const strArray = str.toLowerCase().split("");
-  const positionsArray = [];
+// Original solution;
 
-  strArray.forEach(c => {
-    const index = positions.indexOf(c);
-    if (index !== -1) positionsArray.push(index + 1);
-  });
+// function alphabetPosition(str) {
+//   const positions = "abcdefghijklmnopqrstuvwxyz".split("");
+//   const strArray = str.toLowerCase().split("");
+//   const positionsArray = [];
 
-  return positionsArray.join(" ");
+//   strArray.forEach(c => {
+//     const index = positions.indexOf(c);
+//     if (index !== -1) positionsArray.push(index + 1);
+//   });
+
+//   return positionsArray.join(" ");
+// }
+
+// console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+// 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11
+
+// More elegant solution:
+
+function alphabetPosition(text) {
+  return text
+    .toLowerCase()
+    .match(/[A-Z]/gi)
+    .map(c => c.charCodeAt(0) - 96)
+    .join(" ");
 }
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock."));
-// 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11
+//20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11
