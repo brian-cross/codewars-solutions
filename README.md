0414c828f3ff25607cf018626e24f0bb37dfdc28
Brian Cross <briancross@shaw.ca>
2020-11-24 21:52:45 -0700

    Initial commit. Add readme and first solution

---
+++ README.md
@@ -0,0 +1,3 @@
+## Codewars Solutions - JavaScript
+
+My solutions to the JavaScript challenges on [codewars.com](https://www.codewars.com/)
---
+++ replace-with-alphabet-position/index.js
@@ -0,0 +1,15 @@
+function alphabetPosition(str) {
+  const positions = "abcdefghijklmnopqrstuvwxyz".split("");
+  const strArray = str.toLowerCase().split("");
+  const positionsArray = [];
+
+  strArray.forEach(c => {
+    const index = positions.indexOf(c);
+    if (index !== -1) positionsArray.push(index + 1);
+  });
+
+  return positionsArray.join(" ");
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+// 20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11
